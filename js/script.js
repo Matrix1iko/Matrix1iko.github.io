1d78107b600c24bfa9a2be725d994a4615b4e160
Matrix1iko <81099174+Matrix1iko@users.noreply.github.com>
2022-03-30 18:15:49 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,10 +10,29 @@ var swiper = new Swiper(".mySwiper", {
         modifier: 1,
         slideShadows: true,
         },
+        breakpoints:   {
+            600:{
+                slidesPerView: 3
+        },
+            200:{
+                slidesPerView: 1
+            }
+    }
 });
 
 
 var swiper = new Swiper(".swiper-news", {
     slidesPerView: 3,
     loop: true,
+    breakpoints:   {
+        992:{
+            slidesPerView: 3
+    },
+    500:{
+        slidesPerView: 2
+    },
+    200:{
+        slidesPerView: 1
+    },
+}
 });
